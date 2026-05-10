608950644ac9761dd342ad8c29ef04585aaeccfa
Gaurang Pateriya <51721897+gap6033@users.noreply.github.com>
2026-05-10 20:48:33 +0530

    Add: find-median-in-a-data-stream - submission-1

---
+++ Data Structures & Algorithms/find-median-in-a-data-stream/submission-1.py
@@ -0,0 +1,50 @@
+
+
+class MedianFinder:
+
+    def __init__(self):
+        self.left_side = []
+        self.right_side = []
+
+    def addNum(self, num: int) -> None:
+        if not self.left_side:
+            self.left_side.append(-1 * num)
+        else:
+            if len(self.left_side) - len(self.right_side) == 1:
+                left_greatest = -1 * heapq.heappop(self.left_side)
+                if num > left_greatest:
+                    heapq.heappush(self.right_side, num)
+                    heapq.heappush(self.left_side, -1 * left_greatest)
+                else:
+                    heapq.heappush(self.right_side, left_greatest)
+                    heapq.heappush(self.left_side, -1 * num)
+            else:
+                left_greatest = -1 * heapq.heappop(self.left_side)
+                right_smallest = heapq.heappop(self.right_side)
+                ls = [left_greatest, right_smallest, num]
+                ls.sort()
+                heapq.heappush(self.left_side, -1 * ls[0])
+                heapq.heappush(self.left_side, -1 * ls[1])
+                heapq.heappush(self.right_side, ls[2])
+
+
+    def findMedian(self) -> float:
+        if (len(self.left_side) + len(self.right_side)) % 2 != 0:
+            output = heapq.heappop(self.left_side)
+            heapq.heappush(self.left_side, output)
+            return -1 * output
+        else:
+            left_greatest = heapq.heappop(self.left_side)
+            right_smallest = heapq.heappop(self.right_side)
+            heapq.heappush(self.left_side, left_greatest)
+            heapq.heappush(self.right_side, right_smallest)
+            return (-1 * left_greatest + right_smallest)/2
+
+
+
+        
+
+
+'''
+[2, 3] [5, 7]
+'''
